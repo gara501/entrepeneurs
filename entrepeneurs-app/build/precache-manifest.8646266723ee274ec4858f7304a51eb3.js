3614bc578d7bc952f04db60eebded3f4266f766c
gara501 <gandres.ramirez@gmail.com>
2019-02-10 21:54:14 -0500

    version 1

---
+++ entrepeneurs-app/build/precache-manifest.8646266723ee274ec4858f7304a51eb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a14de0e83d65879646fa",
+    "url": "/static/js/main.a14de0e8.chunk.js"
+  },
+  {
+    "revision": "b14aac1c546927880814",
+    "url": "/static/js/2.b14aac1c.chunk.js"
+  },
+  {
+    "revision": "a14de0e83d65879646fa",
+    "url": "/static/css/main.c1391ed6.chunk.css"
+  },
+  {
+    "revision": "21a3d57e9b257e8c405af9f870ae0bfe",
+    "url": "/index.html"
+  }
+];
