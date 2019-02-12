521db438d06f16bb27d99d23b2d1415a5da166ee
gonzaloramirez <gonzaloramirez@google.com>
2019-02-12 16:58:53 -0500

    Fixed CSS

--- src/App.js
+++ src/App.js
@@ -18,22 +18,58 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({businessData: Data});
     this.setState({cityFilters: this.cityFilters()});
     this.setState({typeFilters: this.typeFilters()});
     this.setState({classificationFilters: this.classificationFilters()});
+    this.getChecked();
+  }
+
+  getChecked() {
+    let filters = document.querySelectorAll('input[type=checkbox]');
+    let checked = {
+      all: [],
+      city: [],
+      type: [],
+      category: []
+    };
+
+    let empty = [].filter.call( filters, function( el ) {
+      return !el.value
+    });
+    let filled = [].filter.call( filters, function( el ) {
+      if (el.checked) {
+        return el.checked
+      }
+    });
+    
+    if (filters.length === empty.length) {
+      this.setState({businessData: Data});
+    } else {
+      checked.all = filled;
+    }
+
+    return checked;
   }
 
   handleChecked (e) {
-    e.persist()
-    let newData = [];
-    this.setState({isChecked: !this.state.isChecked});
+    e.persist();
+    let filters = this.getChecked();
+    if (filters.all.length > 0) {
+
+      filters.forEach(item => {
+        console.log(item);
+      })
+    }
+
+    
+    //this.setState({isChecked: !this.state.isChecked});
+    /*
+    console.log(this.state.isChecked);
     
     switch (e.target.dataset.type) {
       case "city":
         this.state.businessData.map(item => {
           if (item.city === e.target.value) {
-            console.log('CITY', item)
             newData.push(item);
           }
           return item;
@@ -58,18 +94,18 @@ class App extends Component {
       default:
         break;
     }
-    console.log(newData);
+
     if (newData.length <= 0) {
       newData = Data;
     }
     this.setState({businessData: newData});
+    */
   }
 
   moveIntro(e) {
     e.preventDefault();
     let introDiv = document.querySelector('.full-side');
     introDiv.classList.add('out');
-    console.log('The link was clicked.');
   }
 
   cityFilters() {
@@ -103,14 +139,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
+          <button className="menu-mobile-action">
+            <i className="material-icons">menu</i>
+          </button>
           <div className="full-side">
             <div className="intro">
               <h1 className="intro-title">Emprendedores Colombianos, busca lo que necesites, en la ciudad que necesites!</h1>
               <button id="introButton" className="button button-inverse" onClick={this.moveIntro}>Ver Emprendimientos</button>
             </div>
           </div>
-          <div className="left-side">
-            <h2>Filtra tu busqueda:</h2>
+          <div className="left-side">  
+            <h2 className="title">Filtra tu busqueda:</h2>
             <div className="form-block">
               <h4 className="title">Ciudad</h4>
               {this.state.cityFilters.map((item, key) => 
